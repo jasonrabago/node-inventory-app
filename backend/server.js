82081e62420518c19623c446d828936f9094bcd3
Jason <jason@Jasons-MacBook-Air.local>
2022-05-11 22:19:34 -0700

    Added User route and controller

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({extended: false}))
 
 
 app.use('/api/beers', require('./routes/beerRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
